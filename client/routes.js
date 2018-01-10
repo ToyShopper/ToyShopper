aa10d82e6738b49992d300cbf5d03655387cb881
Hunsoo Kim <terlman@gmail.com>
2018-01-10 11:55:07 -0500

    product detail working

--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {Route, Switch, Router} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome, Products} from './components'
+import {Main, Login, Signup, UserHome, Products, ProductDetail} from './components'
 import {me} from './store'
 
 /**
@@ -24,7 +24,8 @@ class Routes extends Component {
             {/* Routes placed here are available to all visitors */}
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
-            <Route path="/products" component={Products} />
+            <Route path="/products/:id" component={ProductDetail} />
+            <Route exact path="/products" component={Products} />
             {
               isLoggedIn &&
                 <Switch>
