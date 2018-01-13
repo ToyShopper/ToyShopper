fb8ab90fc0b65e209bc71c11d9bbc5ef142cb3b8
Hunsoo Kim <terlman@gmail.com>
2018-01-13 17:03:07 -0500

    fixed bug with orders list and single order component views

--- client/components/orders.js
+++ client/components/orders.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchOrders } from '../store/orders';
-import { Card } from 'semantic-ui-react';
+import { Item } from 'semantic-ui-react';
 
 /* -----------------    COMPONENT     ------------------ */
 
@@ -12,37 +12,35 @@ class Orders extends Component {
     this.props.loadOrders();
   }
 
+  renderOrder(order) {
+    return (
+      <Item key={order.id} as={Link} to={'/orders/' + order.id}>
+        <Item.Content>
+          <Item.Header>Order #{order.id}</Item.Header>
+          <Item.Meta>Ordered on {new Date(order.orderedAt).toTimeString()}</Item.Meta>
+          <Item.Description>Order Status: {order.status}</Item.Description>
+          <Item.Description>Order Total: $ {Number(order.total).toFixed(2)}</Item.Description>
+        </Item.Content>
+        {order.user &&
+        (<Item.Content>
+          <Item.Header>User</Item.Header>
+          <Item.Description>{order.user.fullName}</Item.Description>
+        </Item.Content>)
+        }
+      </Item>
+    )
+  }
+
   render() {
     const { orders } = this.props;
     return (
       <div>
         {orders.length > 0 &&
-          <Card.Group>
-          {orders.map(order => (
-            <Card key={order.id} as={Link} to={'/orders/' + order.id}>
-              <Card.Content>
-                <Card.Header>{'Order #: ' + order.id}</Card.Header>
-                <Card.Meta>{'Ordered on ' + order.orderedAt}</Card.Meta>
-                <Card.Description>{'Status: ' + order.status}</Card.Description>
-                <Card.Description>{'Total: ' + order.total}</Card.Description>
-              </Card.Content>
-              <Card.Content>
-                <Card.Header>User</Card.Header>
-                <Card.Meta>{'ID #: ' + order.user.id}</Card.Meta>
-                <Card.Description>{order.user.fullName}</Card.Description>
-              </Card.Content>
-            </Card>
-          ))}
-          </Card.Group>
+          <Item.Group divided>
+            {orders.map(order => this.renderOrder(order))}
+          </Item.Group>
         }
       </div>
-          // <ul>
-          //   {orders.map(order => (
-          //     <li key={order.id}>
-          //     {order.id}</li>
-          //   ))}
-          // </ul>}
-
     )
   }
 }
